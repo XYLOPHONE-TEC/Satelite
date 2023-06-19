9210bb8a4731188d6baa724dcef38b9851890c96
kalungrasuli <kalungirasuli495@gmail.com>
2023-06-19 23:13:15 +0300

    time out

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,17 +1,31 @@
+
+
 function updateClock() {
-    let now = new Date();
-    let hour = now.getHours();
-    let min = now.getMinutes();
-    let sec = now.getSeconds();
+    let  targetDate = new Date('2023-07-30T12:30:00').getTime();
+    
+    let now = new Date().getTime();
+    let distance = targetDate - now;
+    let hour = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));;
+    let min = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));;
+    let sec = Math.floor((distance % (1000 * 60)) / 1000);
+    let days= Math.floor(distance / (1000 * 60 * 60 * 24));
+
+
+    //to string and added a zero if necessary
+     hour =hour.toString().padStart(2, "0");
+      min = min.toString().padStart(2, "0");
+      sec = sec.toString().padStart(2, "0");
+      days = days.toString().padStart(2, "0");
   
     // Add leading zeros if necessary
-    hour = hour.toString().padStart(2, '0');
-    min = min.toString().padStart(2, '0');
-    sec = sec.toString().padStart(2, '0');
-  
-    document.getElementById("hour").textContent = hour;
+    console.log(distance)
+    document.getElementById("hours").textContent = hour;
     document.getElementById("min").textContent = min;
     document.getElementById("sec").textContent = sec;
+    document.getElementById("days").textContent=days;
+
+
+    
   }
   
   // Update the clock immediately
