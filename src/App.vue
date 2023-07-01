6283d15c4a786e72fd183ed660346d7f192ce920
kalungirasuli <kalungirasuli495@gmail.com>
2023-07-01 14:54:49 +0300

    statics

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,46 @@
 <template>
-  <h1></h1>
+  <h1 v-bind:class="ispromoted && ['promoted','new']" v-if="num===13"> promoted</h1>
+  
 </template>
 
 <script>
-
-
 export default {
-  name: 'App',
- data(){
-  return{
-  }
- }
-}
+  name: "App",
+  data() {
+    return {
+      ispromoted:true,
+      num:13
+    };
+  },
+};
 </script>
 
 <style>
-body {
-  margin: 0;
-  padding: 0;
-  background-image: url('../public/fonts/images/background.jpg');
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-position: center center;
-  height: 100vh;
+body{
+  margin:0;
+  padding:0;
+  height: 100%;
 }
 @font-face {
-    font-family: 'Fira Code';
-    src: url('../public/fonts/Fira_Code/FiraCode-VariableFont_wght.ttf') format('truetype');
-    font-weight: bolder;
-  }
+  font-family: "Fira Code";
+  src: url("../public/fonts/Fira_Code/FiraCode-VariableFont_wght.ttf")
+    format("truetype");
+  
+}
+.promoted{
+  color:rgb(0, 255, 64);
+  text-align: center;
+}
+.new{
+  font-size: 150px;
+  font-style: italic;
+  
+}
+.not{
+  color:rgb(51, 153, 76)
+}
 #app {
   font-family: Fira Code, monospace;
-  
 }
 
 </style>
